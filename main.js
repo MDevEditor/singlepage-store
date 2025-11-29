17f534c0d52a4e6fb4101c0361a4b4d5ef017dc5
MDevEditor <mohammadi.7162@gmail.com>
2025-11-29 13:27:05 +0330

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ faqQuestions.forEach((btn) => {
         }
     });
 });
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 40) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
